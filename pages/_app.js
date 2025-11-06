e5890e93d06cb618c678ed46caf116e4ccccfbc3
dilshanmadhuwantha <dilshan.madhuwantha@oncredit.lk>
2025-11-06 16:55:56 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import "react-datepicker/dist/react-datepicker.css";
 import '../styles/globals.css';
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
