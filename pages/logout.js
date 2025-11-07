2411d7c372d1f2a007afd163dcefd1526e548f38
dilshanmadhuwantha <dilshan.madhuwantha@oncredit.lk>
2025-11-07 14:12:10 +0530

    Create logout.js

---
+++ pages/logout.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { supabase } from "../lib/supabase";
+
+export default function Logout() {
+  useEffect(() => {
+    (async () => {
+      await supabase.auth.signOut();
+      window.location.href = "/login";
+    })();
+  }, []);
+
+  return <p>Logging out...</p>;
+}
